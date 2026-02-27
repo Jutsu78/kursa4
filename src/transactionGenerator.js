b7540f7075bc509d845895ccb5d10da1da3219d2
Jutsu78 <loshataraev@gmail.com>
2026-02-27 15:03:37 +0200

    delay between genetaions added

--- src/transactionGenerator.js
+++ src/transactionGenerator.js
@@ -1,10 +1,14 @@
 
-function* generateTransactions() { 
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+async function* generateTransactions() { 
     while (true) {
     const transaction = Math.random() 
   const amount = Math.floor(Math.random() * 1000) + 1
     const currency = ['USD', 'EUR', 'UAH', 'BTC'][Math.floor(Math.random() * 4)]
     yield { transaction, amount, currency}
+
+    await delay(1000)
 }
 
 }
