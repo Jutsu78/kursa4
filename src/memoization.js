d4879a92e84e5eb5e205f84f71aeb34dcdcbab89
Jutsu78 <loshataraev@gmail.com>
2026-02-28 16:39:23 +0200

    memoization key generation now use JSON.stringify

--- src/memoization.js
+++ src/memoization.js
@@ -2,11 +2,12 @@
 function memoize(fn) {
 const cache = new Map();
 return function (...args) {
-const key = generateKey(args);
+const key = JSON.stringify(args);
 if (cache.has(key)) {
+return cache.get(key);
+}
 const result = fn(...args);
 cache.set(key, result);
 return result;
-}
 }
 }
