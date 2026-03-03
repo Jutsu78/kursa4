3e325a25c9ebc233b07dba1be85184b1d6f9ec49
Jutsu78 <loshataraev@gmail.com>
2026-03-03 17:46:00 +0200

    Priority queue integration was completed and generator logic was updated

--- src/transactionGenerator.js
+++ src/transactionGenerator.js
@@ -1,12 +1,13 @@
 
 const memoizedConvert = require('./memoization');
+const priorityQueue = require('./priorityQueue');
 
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 async function* generateTransactions() { 
     while (true) {
     const transaction = Math.random() 
-  const amount = Math.floor(Math.random() * 1000) + 1
+  const amount = Math.floor(Math.random() * 2000) + 1
     const currency = ['USD', 'EUR', 'UAH', 'BTC'][Math.floor(Math.random() * 4)]
     yield { transaction, amount, currency}
 
@@ -19,9 +20,21 @@ async function* generateTransactions() {
 
 async function StartSystem() {
    const liveTransactions = generateTransactions()
+   const queue = new priorityQueue();
+   let tick = 0;
+
     for await (const transaction of liveTransactions) {
       const amountInUAH = memoizedConvert(transaction.amount, transaction.currency);
         console.log('New transaction:', transaction, 'Amount in UAH:', amountInUAH)
+     
+        queue.enqueue(transaction);
+      console.log('added in queue:', transaction.amount, transaction.currency)
+      tick++;
+      if (tick % 2 === 0) {
+       const processedTransaction = queue.dequeue();
+       console.log('Processed transaction:', processedTransaction.amount, processedTransaction.currency);
+      }
+    
     }
   }
 
