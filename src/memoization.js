415a78536570ba242ec912463840e78b936c42be
Jutsu78 <loshataraev@gmail.com>
2026-03-21 15:31:13 +0200

    formatting for code was improved

--- src/memoization.js
+++ src/memoization.js
@@ -1,23 +1,23 @@
 
 function memoize(fn) {
-const cache = new Map();
-return function (...args) {
-const key = JSON.stringify(args);
-if (cache.has(key)) {
-return cache.get(key);
-}
-const result = fn(...args);
-cache.set(key, result);
-return result;
-}
+    const cache = new Map();
+    return function (...args) {
+        const key = JSON.stringify(args);
+        if (cache.has(key)) {
+            return cache.get(key);
+        }
+        const result = fn(...args);
+        cache.set(key, result);
+        return result;
+    }
 }
 
 
-const exchangeRates = {USD: 41.5, EUR: 45.2, BTC: 3500000, UAH: 1};
+const exchangeRates = { USD: 41.5, EUR: 45.2, BTC: 3500000, UAH: 1 };
 function convertor(amount, currency) {
-console.log('Calculating...', amount, currency);
-currency = exchangeRates[currency];
-return amount * currency;
+    console.log('Calculating...', amount, currency);
+    currency = exchangeRates[currency];
+    return amount * currency;
 }
 
 const memoizedConvert = memoize(convertor);
--- src/priorityQueue.js
+++ src/priorityQueue.js
@@ -1,26 +1,26 @@
 
 class priorityQueue {
-constructor() {
-this.vipQueue = [];
-this.regularQueue = [];
-}
+    constructor() {
+        this.vipQueue = [];
+        this.regularQueue = [];
+    }
 
-enqueue(transaction) {
-if (transaction.amount > 1000) {
-this.vipQueue.push(transaction);
- } else {
- this.regularQueue.push(transaction);
- }
-}
- dequeue() {
-if (this.vipQueue.length > 0) {
-return this.vipQueue.shift();
-} else if (this.regularQueue.length > 0) {
-return this.regularQueue.shift();
-}
+    enqueue(transaction) {
+        if (transaction.amount > 1000) {
+            this.vipQueue.push(transaction);
+        } else {
+            this.regularQueue.push(transaction);
+        }
+    }
+    dequeue() {
+        if (this.vipQueue.length > 0) {
+            return this.vipQueue.shift();
+        } else if (this.regularQueue.length > 0) {
+            return this.regularQueue.shift();
+        }
 
-console.log("Queue is empty");
- }
+        console.log("Queue is empty");
+    }
 }
 
 module.exports = priorityQueue;
