9f4befa7b4181a644b5fe7528e20a89cf2efd3ba
Jutsu78 <loshataraev@gmail.com>
2026-04-04 17:42:44 +0300

    memoization function  improved  cache size limit and eviction implemented

--- src/memoization.js
+++ src/memoization.js
@@ -1,17 +1,29 @@
 
 function memoize(fn) {
+   const maxSize = 5;
     const cache = new Map();
     return function (...args) {
         const key = JSON.stringify(args);
         if (cache.has(key)) {
-            return cache.get(key);
+            const value = cache.get(key);
+            cache.delete(key);
+            cache.set(key, value);
+            return value;
         }
         const result = fn(...args);
+
+        if (cache.size >= maxSize) {
+            const oldekey = cache.keys().next().value;
+            cache.delete(oldekey);
+        }
+
+        console.log('caching transaction', args);
+
+
         cache.set(key, result);
         return result;
     }
 }
-
 
 const exchangeRates = { USD: 41.5, EUR: 45.2, BTC: 3500000, UAH: 1 };
 function convertor(amount, currency) {
