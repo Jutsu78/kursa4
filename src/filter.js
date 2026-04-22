d9bf7b55be853250388c6919290d20dba5ce0082
Jutsu78 <loshataraev@gmail.com>
2026-04-22 15:14:31 +0300

    async filter callback with abort signal handling has been implended

--- src/filter.js
+++ src/filter.js
@@ -1,5 +1,16 @@
 const logger = require('./logger');
+
+// callback
 function asyncFilterCallback (array, asyncPredicate, finalCallback, signal) {
     const results = [];
-    let index = 0;
+    let currentIndex = 0;
+
+    function processNext() {
+        if (signal && signal.aborted) {
+            const err = new Error(" [Callback] Operation cancelled");
+            logger.warn({ currentIndex}, "Process stopped with abortcontroller");
+            return finalCallback(err,);
+        }
+    }
 }
+        
