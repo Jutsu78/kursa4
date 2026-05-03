113fc05e66839091a2bff002bc1c2ff4543859ad
Jutsu78 <loshataraev@gmail.com>
2026-05-03 15:48:07 +0300

    subscription management methods were added, formatting for code impoved

--- src/eventEmitter.js
+++ src/eventEmitter.js
@@ -1,7 +1,22 @@
 const logger = require('./logger');
 
 class ReactiveEmitter {
-constructor() {
-    this.events = new Map();
-}
+    constructor() {
+        this.events = new Map();
+    }
+
+    subscribe(eventName, listener) {
+        if (!this.events.has(eventName)) {
+            this.events.set(eventName, []);
+        }
+        this.events.get(eventName).push(listener);
+
+        return () => this.unsubscribe(eventName, listener);
+    }
+
+    unsubscribe(eventName, listener) {
+        if (!this.events.has(eventName)) return;
+        const listeners = this.events.get(eventName).filter(l => l !== listener);
+        this.events.set(eventName, listeners);
+    }
 }
