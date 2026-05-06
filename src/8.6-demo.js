244c43305440f029168b0343c9f85cdf0625c5f7
Jutsu78 <loshataraev@gmail.com>
2026-05-06 21:59:27 +0300

    added dummyTokenProvider for token management in API requests

--- src/8.6-demo.js
+++ src/8.6-demo.js
@@ -3,3 +3,11 @@ const AuthProxy = require('./8.3-authProxy');
 const LoggingProxy = require('./8.4-loggingProxy');
 const { JwtStrategy } = require('./8.2-authStrategies');
 const BankApiService = require('./8.5-bankApiService');
+
+const  dummyTokenProvider = {
+    getToken: async () => process.env.ACCESS_TOKEN ||  "expired_token_123",
+    refreshToken: async () => {
+        console.log('Refreshing token...');
+        process.env.ACCESS_TOKEN = 'new_fresh_token_456';
+    }
+};
