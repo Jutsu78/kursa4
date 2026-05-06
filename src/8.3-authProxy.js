1522634f664ae69c1f1d619fc4b80df9d84e38ba
Jutsu78 <loshataraev@gmail.com>
2026-05-06 20:39:05 +0300

    authentication proxy layer with DI was added

--- src/8.3-authProxy.js
+++ src/8.3-authProxy.js
@@ -8,3 +8,15 @@ class AuthProxy {
     async request(url, options = {}) {
         let authOptions = await this.authStrategy.apply(options);
         let response = await this.innerClient.request(url, authOptions);
+
+        if (response.status === 401 && typeof this.authStrategy.refresh === 'function') {
+            await this.authStrategy.refresh();
+            authOptions = await this.authStrategy.apply(options);
+            response = await this.innerClient.request(url, authOptions);
+        }
+
+        return response;
+    }
+}
+
+module.exports = AuthProxy;
