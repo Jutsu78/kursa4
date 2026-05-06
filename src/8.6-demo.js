77696eb9bd42f4ffc0e77c9ed73e264bed099e85
Jutsu78 <loshataraev@gmail.com>
2026-05-06 21:46:11 +0300

    added roots for demonstration implementation

---
+++ src/8.6-demo.js
@@ -0,0 +1,5 @@
+const BaseHttpClient = require('./8.1-baseClient');
+const AuthProxy = require('./8.3-authProxy');
+const LoggingProxy = require('./8.4-loggingProxy');
+const { JwtStrategy } = require('./8.2-authStrategies');
+const BankApiService = require('./8.5-bankApiService');
